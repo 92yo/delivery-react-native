fec59ff4c2e45a2a1d4dd2e7f7e11964c35a0ff1
yousefr <yousefrizik92@gmail.com>
2021-07-26 20:37:14 +0300

    adds bottom navigation functionality, fixes for iphone-x version and up empty bottom height, svg design for clicking the different options

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
 
+import Tabs from "./navigation/Tabs";
 import { Home, Restaurant, OrderDelivery } from "./screens";
 
 const Stack = createStackNavigator();
@@ -9,12 +10,13 @@ const Stack = createStackNavigator();
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen
-          name="Home"
-          component={Home}
-          options={{ headerShown: false }}
-        />
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+        initialRouteName={"Home"}
+      >
+        <Stack.Screen name="Home" component={Tabs} />
         <Stack.Screen name="Restaurant" component={Restaurant} />
         <Stack.Screen name="OrderDelivery" component={OrderDelivery} />
       </Stack.Navigator>
