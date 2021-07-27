96f913d2d22eb1c664df6157844c9d256c866f03
yousefr <yousefrizik92@gmail.com>
2021-07-27 12:18:00 +0300

    adds mock data, landing page now renders and shows images of restaurants and its properties

--- App.js
+++ App.js
@@ -1,13 +1,24 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
+import { useFonts } from "expo-font";
 
 import Tabs from "./navigation/Tabs";
-import { Home, Restaurant, OrderDelivery } from "./screens";
+import { Restaurant, OrderDelivery } from "./screens";
 
 const Stack = createStackNavigator();
 
 const App = () => {
+  const [loaded] = useFonts({
+    "Roboto-Black": require("./assets/fonts/Roboto-Black.ttf"),
+    "Roboto-Bold": require("./assets/fonts/Roboto-Bold.ttf"),
+    "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
+  });
+
+  if (!loaded) {
+    return null;
+  }
+
   return (
     <NavigationContainer>
       <Stack.Navigator
