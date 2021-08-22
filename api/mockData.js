1612ff3937e5fdfc6848ff8be4c138f7b14df183
yousefr <yousefrizik92@gmail.com>
2021-08-22 15:09:06 +0300

    fixes mockdata long/lat locations for. adds order delivery map with functionality - zoomIn/Out, directions, estTime of arrival, angle of car icon

--- api/mockData.js
+++ api/mockData.js
@@ -3,8 +3,8 @@ import { icons, images } from "../constants";
 export const initialCurrentLocation = {
   streetName: "Tel-Aviv",
   gps: {
-    latitude: 32.109333,
-    longitude: 34.855499,
+    latitude: 32.072014,
+    longitude: 34.784793,
   },
 };
 
@@ -76,8 +76,8 @@ export const restaurantData = [
     photo: images.burger_restaurant_1,
     duration: "30 - 45 min",
     location: {
-      latitude: 1.5347282806345879,
-      longitude: 110.35632207358996,
+      latitude: 32.069596,
+      longitude: 34.783693,
     },
     courier: {
       avatar: images.avatar_1,
@@ -119,8 +119,8 @@ export const restaurantData = [
     photo: images.pizza_restaurant,
     duration: "15 - 20 min",
     location: {
-      latitude: 1.556306570595712,
-      longitude: 110.35504616746915,
+      latitude: 32.073074,
+      longitude: 34.786146,
     },
     courier: {
       avatar: images.avatar_2,
@@ -171,8 +171,8 @@ export const restaurantData = [
     photo: images.hot_dog_restaurant,
     duration: "20 - 25 min",
     location: {
-      latitude: 1.5238753474714375,
-      longitude: 110.34261833833622,
+      latitude: 32.072165,
+      longitude: 34.783078,
     },
     courier: {
       avatar: images.avatar_3,
@@ -198,8 +198,8 @@ export const restaurantData = [
     photo: images.japanese_restaurant,
     duration: "10 - 15 min",
     location: {
-      latitude: 1.5578068150528928,
-      longitude: 110.35482523764315,
+      latitude: 32.070383,
+      longitude: 34.78359,
     },
     courier: {
       avatar: images.avatar_4,
@@ -225,8 +225,8 @@ export const restaurantData = [
     photo: images.noodle_shop,
     duration: "15 - 20 min",
     location: {
-      latitude: 1.558050496260768,
-      longitude: 110.34743759630511,
+      latitude: 32.071656,
+      longitude: 34.784786,
     },
     courier: {
       avatar: images.avatar_4,
@@ -276,8 +276,8 @@ export const restaurantData = [
     photo: images.kek_lapis_shop,
     duration: "35 - 40 min",
     location: {
-      latitude: 1.5573478487252896,
-      longitude: 110.35568783282145,
+      latitude: 32.071334,
+      longitude: 34.784866,
     },
     courier: {
       avatar: images.avatar_1,
---
+++ constants/maps.js
@@ -0,0 +1,3 @@
+const GOOGLE_API_KEY = "AIzaSyCjQlYOLViC-l2B5NhIfqUJm_vnCs6J2yM";
+
+export default GOOGLE_API_KEY;
